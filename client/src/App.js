8d6f97061a3578b7f28407f3da066364676f9aa2
Jacob Lewis <jacobllewis@outlook.com>
2022-06-06 21:03:08 -0600

    revert

--- client/src/App.js
+++ client/src/App.js
@@ -17,33 +17,33 @@ import Profile from './pages/Profile';
 import Protected from './components/Protected';
 import { setContext } from '@apollo/client/link/context';
 
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <UserProvider >
-    {/* <ApolloProvider client={client}> */}
+    <ApolloProvider client={client}>
       <Router>
       <Nav />
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
@@ -75,7 +75,7 @@ function App() {
           </Routes>
         </div>
       </Router>
-    {/* </ApolloProvider> */}
+    </ApolloProvider>
     </UserProvider>
   );
 }
