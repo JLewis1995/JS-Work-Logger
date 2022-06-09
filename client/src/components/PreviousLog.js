2cc910e8ea471462ea08bf2b4696307427eb8387
tedlopus <tedlopus@gmail.com>
2022-06-08 19:44:42 -0600

    updated css

--- client/src/components/PreviousLog.js
+++ client/src/components/PreviousLog.js
@@ -11,10 +11,10 @@ const PreviousLogs = ({ logs }) => {
           // Card to display each previous work log
           <div className="card mb-3">
             <div className="card-body bg-light p-2">
-              <span className="p-3">Name: {log.name}</span>
-              <span className="p-3">Role: {log.role}</span>
-              <span className="p-3">Hours Worked: {log.hours_worked}</span>
-              <span className="p-3">Job Site: {log.job_site}</span>
+              <span className="p-3 text-white">Name: {log.name}</span>
+              <span className="p-3 text-white">Role: {log.role}</span>
+              <span className="p-3 text-white">Hours Worked: {log.hours_worked}</span>
+              <span className="p-3 text-white">Job Site: {log.job_site}</span>
             </div>
           </div>
         ))}
